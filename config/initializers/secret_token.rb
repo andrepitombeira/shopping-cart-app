cd755b08d6edcc3c4051edcee031d0cee9e25643
Schwarzenegger <Schwarzeneggeral@gmail.com>
2013-09-03 16:29:21 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingCartApp::Application.config.secret_token = '2f3ba9e50b17e7a864d68d79622b16071bcb781b9c9465ca06da702ea5bb1066d0486d9e14126d7f433d419eadc6b018eb519e93f8e748401670622db634b108'
