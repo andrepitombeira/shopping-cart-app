8a7e94f6d1a76c4dadc6037c26f7c1e7bb711dbb
Schwarzenegger <Schwarzeneggeral@gmail.com>
2013-09-12 16:27:12 -0300

    add migrations and validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :product, :cart
   belongs_to :product
   belongs_to :cart
+  attr_accessible :cart_id, :product_id, :quantity
+
+  def total_price
+    product.price * quantity
+  end
 end
 
