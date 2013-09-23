c68891c33ac3e1bda92b27d0713daf91c6aacf08
andrepitombeira <andrepitombeira@gmail.com>
2013-09-22 21:15:58 -0300

    finish internationalization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -58,7 +58,7 @@ class OrdersController < ApplicationController
 
         OrderNotifier.received(@order).deliver
 
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render json: @order, status: :created, location: @order }
       else
         @cat = current_cart
